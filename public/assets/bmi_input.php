208afcc9c7c908534df1d1a9324623f26099d32d
Marcin Matuszkiewicz <marcin201@gmail.com>
2023-02-02 11:02:44 -0800

    1.0.0 - template

---
+++ public/assets/bmi_input.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File: BMI input form
+ */
+?>
+
+<div>
+    <div><label for="height">Height</label><input id="height"></div>
+    <div><label for="weight">Weight</label><input id="weight"></div>
+</div>
